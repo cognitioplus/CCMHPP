8f93b22ca759941c0e65cc0635dfe564cbac400c
Cognitio+ <132303904+cognitioplus@users.noreply.github.com>
2025-11-13 03:02:58 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<your-repo-name>/', // ⚠️ Critical for GitHub Pages
+  base: '/cognitioplus.github.io/LMS/', 
 });
